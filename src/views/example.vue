e2f27e347953fef71f4a6fbd7b6d19644e211f4a
Chintoxique <57821149+Chintoxique@users.noreply.github.com>
2021-02-21 17:33:14 +0100

    Ruin update

--- src/views/example.vue
+++ src/views/example.vue
@@ -28,31 +28,44 @@ export default {
     var ville2 = new Image();
     ville2.src = require("../assets/2_GRISEVILLE.png");
 
+    var ville3 = new Image();
+    ville3.src = require("../assets/3_COLLAPSE.png");
+
+    var ville4 = new Image();
+    ville4.src = require("../assets/4_CHAOS.png")
+
     return {
       ville1: ville1,
       ville2: ville2,
-      count: -1,
+      ville3:  ville3,
+      ville4: ville4,
+      count: 0,// -1
       motList : [
-        "Un mot",
+        "Incohérence",
         "Deux mots",
-        "Trois mots"
+        "Trois mots",
       ],
-      mot: "",
+      mot: "Ruin",
       image: ""
     };
   },
   mounted() {
-    this.tick(1)
+    // this.tick(1)
+    this.image = this.ville1.src
   },
   methods : { //THIS.BIATCCCCHH
     getRandomInt(max) {
       return Math.floor(Math.random() * Math.floor(max));
     },
     tick(x) {
-      if (this.count <= 7 * 10) {
-        this.image = this.ville1.src
-      } else if (this.count <= 7 * 11) {
-        this.image = this.ville2.src
+      if (this.count < 77) {
+        this.image = this.ville1.src;
+      } else if (this.count < 154) {
+        this.image = this.ville2.src;
+      } else if (this.count < 231) {
+        this.image = this.ville3.src;
+      } else if (this.count < 308) {
+        this.image = this.ville4.src;
       }
       this.count += x;
       this.mot = this.motList[this.getRandomInt(this.motList.length)]
