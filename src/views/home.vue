40a3d5e3e84fc15d99fe7900d0305e8f07f567ff
sorenanoni <leo.chevalier99@gmail.com>
2021-02-22 02:50:45 +0100

    lint

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,17 +3,37 @@
     class="text-red-900 relative text-center pt-8 container max-w-xs mx-auto"
   >
     <div class="h-screen">
-      <svg v-if='theme == "light"' @click="themeDark()"  class="h-6 w-6 text-white absolute top-0 right-0 martop mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+      <svg
+        v-if="theme == 'light'"
+        @click="themeDark()"
+        class="h-6 w-6 text-white absolute top-0 right-0 martop mr-3"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+        />
       </svg>
-      <svg v-if='theme == "dark"' @click="themeLight()"  class="h-6 w-6 text-black absolute top-0 right-0 martop mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+      <svg
+        v-if="theme == 'dark'"
+        @click="themeLight()"
+        class="h-6 w-6 text-black absolute top-0 right-0 martop mr-3"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+        />
       </svg>
       <div class="h-3"></div>
-      <p class=" px-4 py-2 rounded-none bg-black dark:bg-white text-white dark:text-black font-bold font-sans mx-auto">
+      <p
+        class=" px-4 py-2 rounded-none bg-black dark:bg-white text-white dark:text-black font-bold font-sans mx-auto"
+      >
         {{ mot }}
       </p>
-      <img class="mx-auto absolute top-0 mt-20"  :src="image" alt="ruin" />
+      <img class="mx-auto absolute top-0 mt-20" :src="image" alt="ruin" />
       <button
         class="clicker h-12 w-12 relative focus:outline-none border-b-4 border-red-900 dark:border-shamrock-500 rounded-none bg-black dark:bg-white text-white dark:text-black font-bold font-mono"
         type="button"
@@ -21,23 +41,23 @@
       >
         {{ count }}
       </button>
-      <br>
+      <br />
     </div>
     <div class="">
-      <h1>Music's: </h1>
-      <p><span class="font-bold">77  </span> :  </p>
-      <p><span class="font-bold">154 </span> :  </p>
-      <p><span class="font-bold">231 </span> :  </p>
-      <p><span class="font-bold">308 </span> :  </p>
-      <p><span class="font-bold">385 </span> :  </p>
-      <p><span class="font-bold">462 </span> :  </p>
-      <p><span class="font-bold">539 </span> :  </p>
-      <p><span class="font-bold">616 </span> :  </p>
-      <p><span class="font-bold">693 </span> :  </p>
-      <p><span class="font-bold">770 </span> :  </p>
-      <p><span class="font-bold">847 </span> :  </p>
-      <p><span class="font-bold">924 </span> :  </p>
-      <p><span class="font-bold">1001</span> :  </p>
+      <h1>Music's:</h1>
+      <p><span class="font-bold">77 </span> :</p>
+      <p><span class="font-bold">154 </span> :</p>
+      <p><span class="font-bold">231 </span> :</p>
+      <p><span class="font-bold">308 </span> :</p>
+      <p><span class="font-bold">385 </span> :</p>
+      <p><span class="font-bold">462 </span> :</p>
+      <p><span class="font-bold">539 </span> :</p>
+      <p><span class="font-bold">616 </span> :</p>
+      <p><span class="font-bold">693 </span> :</p>
+      <p><span class="font-bold">770 </span> :</p>
+      <p><span class="font-bold">847 </span> :</p>
+      <p><span class="font-bold">924 </span> :</p>
+      <p><span class="font-bold">1001</span> :</p>
     </div>
   </div>
 </template>
@@ -87,26 +107,49 @@ export default {
     ville13.src = require("../assets/13_pluie.gif");
 
     // Loading music
-    var music1  = new Audio(require('../assets/music/14-Hunger-II.mp3' ));
-    var music2  = new Audio(require('../assets/music/Little-Nightmares-Lure-Of-The-Maw.mp3'));
-    var music3  = new Audio(require('../assets/music/Fly-me-to-the-Moon-Midnight-Version.mp3'));
-    var music4  = new Audio(require('../assets/music/Chilly-Gonzales-Minor-Fantasy.mp3' ));
-    var music5  = new Audio(require('../assets/music/Igorrr-Probleme-d-emotion.mp3' ));
-    var music6  = new Audio(require('../assets/music/Ghost-in-the-Shell-Soundtrack-Making-of-Cyborg.mp3' ));
-    var music7  = new Audio(require('../assets/music/Ergo-Proxy-soundtrack-11-Wasteland-Nostalgia.mp3' ));
-    var music8  = new Audio(require('../assets/music/Little-Nightmares-Prison-Toys.mp3' ));
-    var music9  = new Audio(require('../assets/music/Ib-game-music-medley-20120610-by-YASUpochi.mp3' ));
-    var music10 = new Audio(require('../assets/music/Waltz-With-Bashir-OST-04.-JSB-RPG'));
-    var music11 = new Audio(require('../assets/music/Metropolis-Soundtrack-04_Going_To_Zone.mp3'));
-    var music12 = new Audio(require('../assets/music/Gloomy-Sunday-Billie-Holiday.mp3'));
-    var music13 = new Audio(require('../assets/music/Jigoku-Shoujo-Ost-Kumo-To-Rouba-To-Shoujo.mp3'));
-
+    var music1 = new Audio(require("../assets/music/14-Hunger-II.mp3"));
+    var music2 = new Audio(
+      require("../assets/music/Little-Nightmares-Lure-Of-The-Maw.mp3")
+    );
+    var music3 = new Audio(
+      require("../assets/music/Fly-me-to-the-Moon-Midnight-Version.mp3")
+    );
+    var music4 = new Audio(
+      require("../assets/music/Chilly-Gonzales-Minor-Fantasy.mp3")
+    );
+    var music5 = new Audio(
+      require("../assets/music/Igorrr-Probleme-d-emotion.mp3")
+    );
+    var music6 = new Audio(
+      require("../assets/music/Ghost-in-the-Shell-Soundtrack-Making-of-Cyborg.mp3")
+    );
+    var music7 = new Audio(
+      require("../assets/music/Ergo-Proxy-soundtrack-11-Wasteland-Nostalgia.mp3")
+    );
+    var music8 = new Audio(
+      require("../assets/music/Little-Nightmares-Prison-Toys.mp3")
+    );
+    var music9 = new Audio(
+      require("../assets/music/Ib-game-music-medley-20120610-by-YASUpochi.mp3")
+    );
+    var music10 = new Audio(
+      require("../assets/music/Waltz-With-Bashir-OST-04-JSB-RPG.mp3")
+    );
+    var music11 = new Audio(
+      require("../assets/music/Metropolis-Soundtrack-04_Going_To_Zone.mp3")
+    );
+    var music12 = new Audio(
+      require("../assets/music/Gloomy-Sunday-Billie-Holiday.mp3")
+    );
+    var music13 = new Audio(
+      require("../assets/music/Jigoku-Shoujo-Ost-Kumo-To-Rouba-To-Shoujo.mp3")
+    );
 
     return {
       // Register images
       ville1: ville1,
       ville2: ville2,
-      ville3:  ville3,
+      ville3: ville3,
       ville4: ville4,
       ville5: ville5,
       ville6: ville6,
@@ -132,9 +175,9 @@ export default {
       music12: music12,
       music13: music13,
       // Set the counter
-      count: 0,// -1
+      count: 0, // -1
       // Array (tableau) of string
-      motList : [
+      motList: [
         "Evolution",
         "Progress",
         "Development",
@@ -184,40 +227,44 @@ export default {
         "Transcendance de l'ego",
         "Objet en-soi",
         "Être",
-        "Être pour autrui",
-
+        "Être pour autrui"
       ],
       // Active word
       mot: "Ruin",
       // Active image
       image: "",
-      theme: "",
-    }
+      theme: ""
+    };
   },
   mounted() {
     // this.tick(1)
-    this.image = this.ville1.src
+    this.image = this.ville1.src;
 
     //theme setup
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark')
-      this.theme = "dark"
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+      this.theme = "dark";
     } else {
-      document.documentElement.classList.remove('dark')
-      this.theme = "light"
+      document.documentElement.classList.remove("dark");
+      this.theme = "light";
     }
   },
-  methods : { //THIS.BIATCCCCHH
+  methods: {
+    //THIS.BIATCCCCHH
     // Theme action
     themeLight() {
-      document.documentElement.classList.remove('dark')
-      localStorage.theme = 'light'
-      this.theme = "light"
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "light";
+      this.theme = "light";
     },
     themeDark() {
-      document.documentElement.classList.add('dark')
-      localStorage.theme = 'dark'
-      this.theme = "dark"
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "dark";
+      this.theme = "dark";
     },
     // Generate a rand number
     getRandomInt(max) {
@@ -227,137 +274,125 @@ export default {
       if (this.count < 77) {
         this.image = this.ville1.src;
         if (this.count == 1) {
-          this.music1.currentTime = 0
-          this.music1.loop = true
-          this.music1.volume = 0.8
-          this.music1.play()
+          this.music1.currentTime = 0;
+          this.music1.loop = true;
+          this.music1.volume = 0.8;
+          this.music1.play();
         }
       } else if (this.count < 154) {
         this.image = this.ville2.src;
         if (this.count == 77) {
-          this.music1.pause()
-          this.music2.currentTime = 0
-          this.music2.loop = true
-          this.music2.volume = 0.8
-          this.music2.play()
+          this.music1.pause();
+          this.music2.currentTime = 0;
+          this.music2.loop = true;
+          this.music2.volume = 0.8;
+          this.music2.play();
         }
-
       } else if (this.count < 231) {
         this.image = this.ville3.src;
         if (this.count == 154) {
-          this.music2.pause()
-          this.music3.currentTime = 0
-          this.music3.loop = true
-          this.music3.volume = 0.8
-          this.music3.play()
+          this.music2.pause();
+          this.music3.currentTime = 0;
+          this.music3.loop = true;
+          this.music3.volume = 0.8;
+          this.music3.play();
         }
-
       } else if (this.count < 308) {
         this.image = this.ville4.src;
         if (this.count == 231) {
-          this.music3.pause()
-          this.music4.currentTime = 0
-          this.music4.loop = true
-          this.music4.volume = 0.8
-          this.music4.play()
+          this.music3.pause();
+          this.music4.currentTime = 0;
+          this.music4.loop = true;
+          this.music4.volume = 0.8;
+          this.music4.play();
         }
-
-      } else if (this.count < 385 ) {
+      } else if (this.count < 385) {
         this.image = this.ville5.src;
         if (this.count == 308) {
-          this.music4.pause()
-          this.music5.currentTime = 0
-          this.music5.loop = true
-          this.music5.volume = 0.8
-          this.music5.play()
+          this.music4.pause();
+          this.music5.currentTime = 0;
+          this.music5.loop = true;
+          this.music5.volume = 0.8;
+          this.music5.play();
         }
-
       } else if (this.count < 462) {
-        this.image = this.ville6.src
+        this.image = this.ville6.src;
         if (this.count == 385) {
-          this.music5.pause()
-          this.music6.currentTime = 0
-          this.music6.loop = true
-          this.music6.volume = 0.8
-          this.music6.play()
+          this.music5.pause();
+          this.music6.currentTime = 0;
+          this.music6.loop = true;
+          this.music6.volume = 0.8;
+          this.music6.play();
         }
-
       } else if (this.count < 539) {
-        this.image = this.ville7.src
+        this.image = this.ville7.src;
         if (this.count == 462) {
-          this.music6.pause()
-          this.music7.currentTime = 0
-          this.music7.loop = true
-          this.music7.volume = 0.8
-          this.music7.play()
+          this.music6.pause();
+          this.music7.currentTime = 0;
+          this.music7.loop = true;
+          this.music7.volume = 0.8;
+          this.music7.play();
         }
-
       } else if (this.count < 616) {
-        this.image = this.ville8.src
+        this.image = this.ville8.src;
         if (this.count == 539) {
-          this.music7.pause()
-          this.music8.currentTime = 0
-          this.music8.loop = true
-          this.music8.volume = 0.8
-          this.music8.play()
+          this.music7.pause();
+          this.music8.currentTime = 0;
+          this.music8.loop = true;
+          this.music8.volume = 0.8;
+          this.music8.play();
         }
-
       } else if (this.count < 693) {
-        this.image = this.ville9.src
+        this.image = this.ville9.src;
         if (this.count == 616) {
-          this.music8.pause()
-          this.music9.currentTime = 0
-          this.music9.loop = true
-          this.music9.volume = 0.8
-          this.music9.play()
+          this.music8.pause();
+          this.music9.currentTime = 0;
+          this.music9.loop = true;
+          this.music9.volume = 0.8;
+          this.music9.play();
         }
-
       } else if (this.count < 770) {
-        this.image = this.ville10.src
+        this.image = this.ville10.src;
         if (this.count == 693) {
-          this.music9.pause()
-          this.music10.currentTime = 0
-          this.music10.loop = true
-          this.music10.volume = 0.8
-          this.music10.play()
+          this.music9.pause();
+          this.music10.currentTime = 0;
+          this.music10.loop = true;
+          this.music10.volume = 0.8;
+          this.music10.play();
         }
-
       } else if (this.count < 847) {
-        this.image = this.ville11.src
+        this.image = this.ville11.src;
         if (this.count == 770) {
-          this.music10.pause()
-          this.music11.currentTime = 0
-          this.music11.loop = true
-          this.music11.volume = 0.8
-          this.music11.play()
+          this.music10.pause();
+          this.music11.currentTime = 0;
+          this.music11.loop = true;
+          this.music11.volume = 0.8;
+          this.music11.play();
         }
-
       } else if (this.count < 924) {
-        this.image = this.ville12.src
+        this.image = this.ville12.src;
         if (this.count == 847) {
-          this.music11.pause()
-          this.music12.currentTime = 0
-          this.music12.loop = true
-          this.music12.volume = 0.8
-          this.music12.play()
+          this.music11.pause();
+          this.music12.currentTime = 0;
+          this.music12.loop = true;
+          this.music12.volume = 0.8;
+          this.music12.play();
         }
-
       } else if (this.count < 1001) {
-        this.image = this.ville13.src
+        this.image = this.ville13.src;
         if (this.count == 924) {
-          this.music12.pause()
-          this.music13.currentTime = 0
-          this.music13.loop = true
-          this.music13.volume = 0.8
-          this.music13.play()
+          this.music12.pause();
+          this.music13.currentTime = 0;
+          this.music13.loop = true;
+          this.music13.volume = 0.8;
+          this.music13.play();
         }
-
       }
       this.count += x;
-      this.mot = this.motList[this.getRandomInt(this.motList.length)]
+      this.mot = this.motList[this.getRandomInt(this.motList.length)];
     }
   }
-}
+};
 </script>
 
 <style lang="css">
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   pwa: {
-    themeColor: '#000000',
-    name: 'RUIN'
+    themeColor: "#000000",
+    name: "RUIN"
   }
-}
+};
